e3bd67e304c5254d496858fa95d57a67a74d4977
Denverth Jhay <denverthjhay.larida.21@usjr.edu.ph>
2022-12-03 17:41:27 +0800

    updatedGetstarted Screen

--- digital_aid_nurse/lib/getstarted.dart
+++ digital_aid_nurse/lib/getstarted.dart
@@ -9,76 +9,58 @@ class GetstartedscreenWidget extends StatefulWidget {
 class _GetstartedscreenWidgetState extends State<GetstartedscreenWidget> {
   @override
   Widget build(BuildContext context) {
-    return Material(
-        child: Stack(children: <Widget>[
-      Positioned(
-        top: 7,
-        left: 11,
-        child: Container(),
-      ),
-      Positioned(
-          top: 205,
-          left: 52,
-          child: Container(
-              width: 270,
-              height: 254,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/images/Rectangle87.png'),
-                    fit: BoxFit.fitWidth),
-              ))),
-      Positioned(
-        top: 658,
-        left: 38,
-        child: TextButton(
-          child: Container(
-              width: 300,
-              height: 60,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20),
+    return Scaffold(
+        body: Padding(
+            padding: EdgeInsets.all(50),
+            child: Column(children: <Widget>[
+              const SizedBox(
+                height: 80,
+              ),
+              Container(
+                  width: 270,
+                  height: 254,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/Rectangle87.png'),
+                        fit: BoxFit.fitWidth),
+                  )),
+              const SizedBox(
+                height: 140,
+              ),
+              Container(
+                height: 60,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: Color.fromRGBO(6, 174, 213, 1),
                 ),
-                color: Color.fromRGBO(6, 174, 213, 1),
-              )),
-          onPressed: () => {},
-        ),
-      ),
-      Positioned(
-          top: 674,
-          left: 129,
-          child: TextButton(
-            child: Text(
-              'Get Started',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                  color: Color.fromRGBO(255, 255, 255, 1),
-                  fontFamily: 'DM Sans',
-                  fontSize: 23,
-                  letterSpacing: 0,
-                  fontWeight: FontWeight.normal,
-                  height: 1),
-            ),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => loginscreenWidget()),
-              );
-            },
-          )),
-      Positioned(
-          top: 730,
-          left: 96,
-          child: Container(
-              width: 169,
-              height: 13,
-              child: Stack(children: <Widget>[
-                Positioned(
-                    top: 0,
-                    left: 0,
-                    child: Text(
+                child: MaterialButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => loginscreenWidget()),
+                    );
+                  },
+                  child: const Text(
+                    "Get Started",
+                    style: TextStyle(
+                      fontFamily: 'DM Sans',
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                  width: 169,
+                  height: 13,
+                  child: Stack(children: <Widget>[
+                    Text(
                       'Already have an account? ',
                       textAlign: TextAlign.left,
                       style: TextStyle(
@@ -89,23 +71,23 @@ class _GetstartedscreenWidgetState extends State<GetstartedscreenWidget> {
                               0 /*percentages not used in flutter. defaulting to zero*/,
                           fontWeight: FontWeight.normal,
                           height: 1),
-                    )),
-                Positioned(
-                    top: 0,
-                    left: 137,
-                    child: Text(
-                      'Log in',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                          color: Color.fromRGBO(6, 174, 213, 1),
-                          fontFamily: 'DM Sans',
-                          fontSize: 11,
-                          letterSpacing:
-                              0 /*percentages not used in flutter. defaulting to zero*/,
-                          fontWeight: FontWeight.normal,
-                          height: 1),
-                    )),
-              ]))),
-    ]));
+                    ),
+                    Positioned(
+                        top: 0,
+                        left: 137,
+                        child: Text(
+                          'Log in',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              color: Color.fromRGBO(6, 174, 213, 1),
+                              fontFamily: 'DM Sans',
+                              fontSize: 11,
+                              letterSpacing:
+                                  0 /*percentages not used in flutter. defaulting to zero*/,
+                              fontWeight: FontWeight.normal,
+                              height: 1),
+                        )),
+                  ])),
+            ])));
   }
 }
